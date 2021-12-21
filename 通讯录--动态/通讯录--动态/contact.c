6c1f268cab9a42708b93b81c8d6063b66338c6d3
rookie_bc <2956150447@qq.com>
2021-12-21 22:41:11 +0800

    动态通讯录

---
+++ 通讯录--动态/通讯录--动态/contact.c
@@ -0,0 +1,211 @@
+//函数的定义
+#include "contact.h"
+
+
+void InitContact(struct Contact* ps)
+{
+	//memset(ps->data, 0, sizeof(ps->data));
+	//ps->size = 0;//设置通讯录最初只有0个元素      //动态版本
+	ps->data = (struct PeoInfo*)malloc(DEFAULT_SZ * sizeof(struct PeoInfo));
+	if (ps->data == NULL)
+	{
+		return 0;
+	}
+	ps->size = 0;
+	ps->capacity = DEFAULT_SZ;
+}
+
+void CheckCapacity(struct Contact* ps)
+{
+	if (ps->size == ps->capacity)
+	{
+		struct PeoInfo* ptr = (struct PeoInfo*)realloc(ps->data, (ps->capacity + 2) * sizeof(struct PeoInfo));
+		if (ptr != NULL)
+		{
+			ps->data = ptr;
+			ps->capacity += 2;
+			printf("增容成功\n");
+		}
+		else
+		{
+			printf("增容失败\n");
+		}
+	}
+}
+
+//   静态
+//void AddContact(struct Contact* ps)
+//{
+//	if (ps->size == MAX)
+//	{
+//		printf("通讯录已满，无法增加！\n");
+//	}
+//	else
+//	{
+//		printf("请输入名字：");
+//		scanf("%s", ps->data[ps->size].name);   //找到数组data中的某一个元素的名字位置
+//		printf("请输入年龄：");
+//		scanf("%d", &ps->data[ps->size].age);
+//		printf("请输入性别：");
+//		scanf("%s", ps->data[ps->size].sex);
+//		printf("请输入电话：");
+//		scanf("%s", ps->data[ps->size].tele);
+//		printf("请输入地址：");
+//		scanf("%s", ps->data[ps->size].addr);
+//
+//		ps->size++;            //放进去一个好友  就加加
+//		printf("添加成功！\n");
+//	}
+//}
+
+//动态
+void AddContact(struct Contact* ps)
+{
+	CheckCapacity(ps);         //检验容量是否需要扩容
+
+
+	printf("请输入名字：");
+	scanf("%s", ps->data[ps->size].name);   //找到数组data中的某一个元素的名字位置
+	printf("请输入年龄：");
+	scanf("%d", &ps->data[ps->size].age);
+	printf("请输入性别：");
+	scanf("%s", ps->data[ps->size].sex);
+	printf("请输入电话：");
+	scanf("%s", ps->data[ps->size].tele);
+	printf("请输入地址：");
+	scanf("%s", ps->data[ps->size].addr);
+
+	ps->size++;            //放进去一个好友  就加加
+	printf("添加成功！\n");
+
+}
+
+void ShowContact(const struct Contact* ps)
+{
+	if (ps->size == 0)
+	{
+		printf("您未添加好友!\n");
+	}
+	else
+	{
+		printf("%-12s\t%-4s\t%-4s\t%-12s\t%-20s\n", "名字", "年龄", "性别", "电话", "地址");
+		int i = 0;
+		for (i = 0; i < ps->size; i++)
+		{
+			printf("%-12s\t%-4d\t%-4s\t%-12s\t%-20s\n",
+				ps->data[i].name,
+				ps->data[i].age,
+				ps->data[i].sex,
+				ps->data[i].tele,
+				ps->data[i].addr);
+		}
+	}
+}
+
+
+static int FindByName(const struct Contact* ps, char name[MAX_NAME])         //被static修饰只能在改文件下用
+{
+	int i;
+	for (i = 0; i < ps->size; i++)
+	{
+		if (0 == strcmp(ps->data[i].name, name))        //查找某一个名字在通讯录中的位置
+		{
+			return i;                                   //找到了返回下标
+		}
+	}
+	return -1;               //找不到返回-1
+}
+void DelContact(struct Contact* ps)
+{
+	char name[MAX_NAME];
+	printf("请输入删除人姓名:");
+	scanf("%s", name);
+	int pos = FindByName(ps, name);  //找到了名字所在元素的下标    找不到返回-1
+	if (pos==-1)
+	{
+		printf("未找到要删除人的信息！\n");
+	}
+	else
+	{
+		int j;
+		for (j = pos; j < ps->size - 1; j++)
+		{
+			ps->data[j] = ps->data[j + 1];                     //2.删除   将后一个元素将前一个元素覆盖
+		}
+		ps->size--;
+		printf("删除成功！\n");
+	}
+}
+
+
+
+void SearchContact(const struct Contact* ps)
+{
+	char name[MAX_NAME];
+	printf("请输入要查找人的姓名:");
+	scanf("%s", name);
+	int pos = FindByName(ps, name);
+	if (pos == -1)
+	{
+		printf("未找到要查找人的信息！\n");
+	}
+	else
+	{
+		printf("%-12s\t%-4s\t%-4s\t%-12s\t%-20s\n", "名字", "年龄", "性别", "电话", "地址");
+		printf("%-12s\t%-4d\t%-4s\t%-12s\t%-20s\n",
+			ps->data[pos].name,
+			ps->data[pos].age,
+			ps->data[pos].sex,
+			ps->data[pos].tele,
+			ps->data[pos].addr);
+	}
+}
+
+
+void ModifyContact(struct Contact* ps)
+{
+	char name[MAX_NAME];
+	printf("请输入要修改人的姓名:");
+	scanf("%s", name);
+	int pos = FindByName(ps, name);
+	if (pos == -1)
+	{
+		printf("未找到要修改人的信息！\n");
+	}
+	else
+	{
+		printf("请输入名字：");
+		scanf("%s", ps->data[pos].name);   
+		printf("请输入年龄：");
+		scanf("%d", &ps->data[pos].age);
+		printf("请输入性别：");
+		scanf("%s", ps->data[pos].sex);
+		printf("请输入电话：");
+		scanf("%s", ps->data[pos].tele);
+		printf("请输入地址：");
+		scanf("%s", ps->data[pos].addr);
+
+		printf("修改成功\n");
+	}
+}
+
+
+
+//        排序 
+int cmp_peo_by_name(const void* e1, const void* e2)
+{
+	return strcmp((*(PeoInfo*)e1).name, (*(PeoInfo*)e2).name);
+}
+
+void SortContact(struct Contact* ps)
+{
+	qsort(ps->data->name,ps->size, sizeof(PeoInfo), cmp_peo_by_name);
+}
+
+
+
+void DestroyContact(Contact* ps)
+{
+	free(ps->data);
+	ps->data = NULL;
+}
---
+++ 通讯录--动态/通讯录--动态/contact.h
@@ -0,0 +1,62 @@
+#pragma once
+// 头文件的引用
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>   //memset
+#include <stdlib.h>
+
+
+//#define MAX 1000          //静态版本下的
+
+
+#define DEFAULT_SZ 3
+#define MAX_NAME 20
+#define MAX_SEX 5
+#define MAX_TELE 12
+#define MAX_ADDR 30
+
+ 
+enum Option                 //枚举   令菜单的选项更好读
+{
+	EXIT,
+	ADD,
+	DEL,
+	SEARCH,
+	MODIFY,
+	SHOW,
+	SORT
+};
+
+//结构体的声明
+typedef struct PeoInfo              //人的信息
+{
+	char name[MAX_NAME];
+	int age;
+	char sex[MAX_SEX];
+	char tele[MAX_TELE];
+	char addr[MAX_ADDR];
+}PeoInfo;
+//通讯录类型
+typedef struct Contact
+{
+	struct PeoInfo* data;        //存放好友信息的空间   需要动态开辟
+	int size;                    //记录当前已经有多少个人
+	int capacity;                //记录通讯录的容量
+}Contact;
+
+//函数的声明
+void InitContact(struct Contact* ps);						     //初始化通讯录的函数
+	
+void AddContact(struct Contact* ps);						     //增加好友信息的函数
+
+void DelContact(struct Contact* ps);				  	         //删除好友信息的函数
+
+void SearchContact(const struct Contact* ps);				     //查找好友信息的函数
+  
+void ModifyContact(struct Contact* ps);                          //修改好友信息的函数
+
+void ShowContact(const struct Contact* ps);						 //显示通讯录信息的函数
+
+void SortContact(struct Contact* ps);                            //将通讯录排序的函数
+
+void DestroyContact(Contact* ps);                                 //释放动态内存的函数
